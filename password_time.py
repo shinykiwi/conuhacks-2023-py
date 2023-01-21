82ee59b00a54a46c390980a393e8ea51101d99f6
lucasadilla <58784755+lucasadilla@users.noreply.github.com>
2023-01-21 12:05:34 -0500

    new password policy library

--- password_time.py
+++ password_time.py
@@ -1,9 +1,15 @@
-import password_strength
+from passlib.pwd import PasswordPolicy, crack_time_seconds
 
 def calculate_crack_time(password):
-    strength = password_strength.password_strength(password)
-    time_to_crack = strength.crack_time
-    return time_to_crack
+    policy = PasswordPolicy.from_names(
+        length=8,  # Minimal length for the password
+        uppercase=1,  # Minimal amount of uppercase characters
+        digits=1,  # Minimal amount of digits
+        nonletters=1,  # Minimal amount of non-letter characters
+    )
+
+    crack_time = crack_time_seconds(password, policy)
+    return crack_time
 
 password = "mypassword123"
 crack_time = calculate_crack_time(password)
